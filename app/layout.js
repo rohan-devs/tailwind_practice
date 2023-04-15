c0e40668a30565b713c43172934e1891b61f50c3
rohanwebuwu <rohanshah6982@gmail.com>
2023-04-15 19:59:14 +0530

    deploy

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'tailwind seekhenge aaj toth',
+  description: 'kya bolti publiccccc',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.js
@@ -0,0 +1,149 @@
+
+import { Inter } from 'next/font/google'
+
+
+// const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+   <>
+  
+holaelk lkre 
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, repellat iure. Veritatis, eveniet voluptatibus nostrum doloribus aliquam, eos earum aspernatur, animi fuga praesentium blanditiis explicabo! Aut ipsa odio maiores velit!
+<p className='text-sm xl:text-5xl lg:text-lg'>riuvnurwnuicfnwiuo</p>
+<p className='text-lg mb-2'>riuvnurwnuicfnwiuo</p>
+<p>Lorem ipsum dolor sit amet consectetur.
+</p>
+<div className="bg-[#bada55]">
+  worinio3rnfio3nrwiofni3nf
+  <p className='mb-5'>iwncwrinciown</p>
+  wirncwri
+</div>
+<div className='text-black bg-[#FF6D60]'>
+  <p className="text-xl text-[#301311]">
+    qeojuncqouew
+  </p>
+  <p className="font-sans" >
+    Lorem ipsum dolor sit amet consectetur adipisicing.
+  </p>
+  <p className="bg-[#B9EDDD] text-center hover:bg-[#87CBB9]">I am hoverable brooo</p>
+<p className="italic">i am italic</p>
+<p className="line-through">i am muslim(katwa)</p>
+<p className="overline">overline test</p>
+<p className="underline decoration-slate-400 decoration-wavy decoration-8 underline-offset-8 mb-4 pb-3 lowercase">underline with text decorUEHFUWation</p>
+<p className="underline decoration-slate-400 decoration-wavy decoration-8 underline-offset-8 mb-4 pb-3 uppercase">underline with text decorUEHFUWation</p>
+
+
+
+
+</div>
+<div className="bg-[#3C486B] break-all">
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem officiis et assumenda quibusdam non. Repellendus laborum praesentium nemo iure officiis non officia sed corporis ducimus perspiciatis? Ipsam explicabo inventore nostrum incidunt provident aliquam accusamus dolores? Repellat veniam placeat voluptates corporis sit cumque nisi, expedita recusandae ducimus oirnvioneruifcn3runfocin3weiofnc3woenrfo3wncfion3reofnco3wur dicta sapiente modi architecto assumenda id? Nam atque eius optio dolor earum laborum, facilis consequatur maxime fuga doloribus officia. Voluptate dolorum aperiam incidunt.</p>
+</div>
+<h1 className="bg-red-500 p-2 text-black text-center font-bold ">
+Hello
+</h1>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut culpa porro dicta deleniti est unde voluptatem beatae dolorum magni eius nulla quibusdam dolorem quisquam, doloribus placeat, error hic ea quos consequatur distinctio sequi odio? Ipsum consequatur cum labore accusamus officiis magnam architecto quidem ea repellat?
+</p>
+ 
+  <div className="flex">
+  <div className='bg-red-500 w-1/6 h-min-h font-extrabold font-serif text-black text-center justify-center '>side</div>
+  <div className='bg-green-500 w-5/6 h-16 text-center '>main</div>
+  </div>
+  <div className="flex">
+    <div className="basis-4/12  bg-slate-600 text-center">1</div>
+    <div className="basis-4/12 bg-slate-700 text-center">2</div>
+    <div className="basis-4/12 bg-slate-800 text-center">3</div>
+  </div>
+  <div className=" flex  flex-nowrap  md:flex-wrap w-full">
+    <div className="basis-4/12  bg-slate-600 text-center">1b</div>
+    <div className="w-32 md:w-full bg-slate-700 text-center">2b</div>
+    <div className="w-full md:w-32 bg-slate-800 text-center">3b</div>
+  </div>
+  <div className=" flex flex-nowrap md:flex-wrap w-full">
+    <div className="w-full  bg-slate-600 text-center">1b</div>
+    <div className="w-full bg-slate-700 text-center">2b</div>
+    <div className="w-full bg-slate-800 text-center">3b</div>
+  </div>
+   <div className='flex bg-red-500 uppercase justify-evenly items-center  '>
+    <p className=''>i am at the center bro </p>
+    <p>i am at the center bro
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, autem neque ullam voluptatum consectetur nobis corrupti saepe impedit recusandae molestiae vel distinctio enim iusto molestias quos sit, magni similique architecto inventore doloremque consequuntur totam? Vero repudiandae unde corporis. Dolor corrupti hic iste quas dignissimos! Praesentium.</p> </p>
+    <p>i am at the center bro </p>
+    <p>i am at the center bro </p>
+   </div>
+   <div className="grid gap-4 grid-cols-1 sm:grid-cols-2  text-center">
+    <p className=' bg-sky-200 rounded-sm'>div1</p>
+    <p className=' bg-sky-200 rounded-sm'>div2</p>
+    <p className=' bg-sky-200 rounded-sm col-span-2 sm:bg-red-500'>div3</p>
+    <p className=' bg-sky-200 rounded-sm'> div4</p>
+    <p className=' bg-sky-200 rounded-sm'>div5</p>
+    <p className=' bg-sky-200 rounded-sm'>div6</p>
+   </div>
+<div className=' container overflow-visible  bg-red-500 p-9 justify-center'>
+  <img classname='' src="https://images.pexels.com/photos/16294242/pexels-photo-16294242.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt="" />
+  <p className='text-[20px]'>
+  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, recusandae! Ipsam quibusdam, velit natus delectus inventore deserunt qui. Aliquam ipsam odit perferendis numquam quisquam nemo culpa nulla, nam quaerat unde. Dicta officiis natus eaque aspernatur, velit reprehenderit animi recusandae, obcaecati molestiae hic necessitatibus corrupti omnis nisi, accusantium nemo. Quidem deserunt rem amet praesentium, architecto accusamus aperiam aspernatur asperiores quia porro voluptate, et veritatis modi, nisi blanditiis quos nostrum quam voluptates non provident. Distinctio porro ea nisi doloribus, voluptate corporis vel error, laboriosam enim odio impedit animi consequatur voluptatum. Labore beatae asperiores provident quisquam iure molestias, dicta incidunt odio animi rerum?
+  </p>
+</div>
+<div className="container flex">
+<div class="z-40 bg-red-400 rounded-full border border-white p-8 ">05</div>
+<div class="z-30 bg-red-400 rounded-full border border-white p-8 ">04</div>
+<div class="z-20 bg-red-400 rounded-full border border-white p-8 ">03</div>
+<div class="z-10 bg-red-400 rounded-full border border-white p-8 ">02</div>
+<div class="z-0 bg-red-400 rounded-full border border-white p-8 ">01</div>
+</div>
+<div className="container flex bg-slate-100">
+<img
+className=' w-96 h-96 object-left-top ' 
+src="https://images.pexels.com/photos/16294242/pexels-photo-16294242.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt="" />
+</div>
+<div className="h-full text-white bg-slate-400 justify-center">
+  <button className=' outline outline-offset-2 outline-8 my-4 mx-4 outline-cyan-500'> click me </button>
+</div>
+<div className="h-full text-white bg-slate-400 justify-center">
+  <button className=' ring-4 ring-offset-2'> click me </button>
+</div>
+<div className="flex  w-full justify-center ">
+  <img src="https://images.pexels.com/photos/16294242/pexels-photo-16294242.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt="" className=" blur-md w-32 h-32 hover:blur-none hover: border-r-4 hover:brightness-150" />
+  <img src="https://images.pexels.com/photos/16294242/pexels-photo-16294242.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt="" className=" hover:brightness-50 w-32 h-32" />
+</div>
+
+<div className=" bg-cyan-700 flex  font-serif font-bold flex-col col-span-1 p-4 justify-evenly">
+  <button className=' border-3 bg-yellow-300 rounded-lg text-black hover:translate-x-2 transition '>
+    Read more
+  </button>
+  <button className=' border-3 bg-yellow-300 rounded-lg text-black my-2 hover:translate-x-2 transition delay-1000 '>
+    see the delay 1000ms
+  </button>
+  <button className=' border-3 bg-yellow-300 rounded-lg text-black my-2 hover:translate-x-2 transition  duration-1000 '>
+    see the duration 1000ms
+  </button>
+  <button className=' border-3 bg-yellow-300 rounded-lg text-black my-2 hover:translate-x-2  easi-in duration-300 '>
+    ease in
+  </button>
+  <button className=' border-3 bg-yellow-300 rounded-lg text-black my-2 hover:translate-x-2   ease-out duration-300 '>
+    ease out
+  </button>
+  <button className=' border-3 bg-yellow-300 rounded-lg text-black my-2 hover:translate-x-2   ease-in-out duration-300 '>
+    ease-in-out
+  </button>
+  <button className=' border-3 bg-yellow-300 rounded-lg text-black animate-bounce '>
+    bounce
+  </button>
+  <button className=' border-3 bg-yellow-300 rounded-lg text-black hover:animate-spin transition duration-300'>
+    hover to spin
+  </button>
+  <button className=' border-3 bg-yellow-300 rounded-lg text-black  transition hover:skew-x-12 m-4'>
+    hover to skew
+  </button>
+  <div className='text-center'>
+    <a href="www.youtube.com" className=''>YOUTUBE</a>
+  </div>
+
+
+</div>
+   </>
+  )
+}
